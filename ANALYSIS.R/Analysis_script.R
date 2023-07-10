23a957285e6139e78b809ef0f6fe5ab242f7dac2
Bao Tran Cao <135206055+baotranc2000@users.noreply.github.com>
2023-07-10 17:52:41 +0700

    Analysis_script.R

--- ANALYSIS.R/Analysis_script.R
+++ ANALYSIS.R/Analysis_script.R
@@ -1,60 +1,55 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#First, check the cleaned data frame and summarised detail of cleaned dataset
+str(tripdata_cleaned)
+summary(tripdata_cleaned)
 
-\f0\fs24 \cf0 #Load necessary packages & library\
-install.packages("tidyverse")\
-install.packages("janitor")\
-install.packages("lubridate")\
-library(janitor)\
-library(tidyverse)\
-library(lubridate)\
-library(dplyr)\
-library(readr)\
-\
-#import data in R studio\
-q6_2022 <- read_csv("q6_2022.csv")\
-q7_2022 <- read_csv("q7_2022.csv")\
-q8_2022 <- read_csv("q8_2022.csv")\
-q9_2022 <- read_csv("q9_2022.csv")\
-q10_2022 <- read_csv("q10_2022.csv")\
-q11_2022 <- read_csv("q11_2022.csv")\
-q12_2022 <- read_csv("q12_2022.csv")\
-q1_2023 <- read_csv("q1_2023.csv")\
-q2_2023 <- read_csv("q2_2023.csv")\
-q3_2023 <- read_csv("q3_2023.csv")\
-q4_2023 <- read_csv("q4_2023.csv")\
-q5_2023 <- read_csv("q5_2023.csv")\
-\
-#Check data sets for consistency \
-colnames(q6_2022)\
-colnames(q7_2022)\
-colnames(q8_2022)\
-colnames(q9_2022)\
-colnames(q10_2022)\
-colnames(q11_2022)\
-colnames(q12_2022)\
-colnames(q1_2023)\
-colnames(q2_2023)\
-colnames(q3_2023)\
-colnames(q4_2023)\
-colnames(q5_2023)\
-#Check structure of each dataset\
-str(q6_2022)\
-str(q7_2022)\
-str(q8_2022)\
-str(q9_2022)\
-str(q10_2022)\
-str(q11_2022)\
-str(q12_2022)\
-str(q1_2023)\
-str(q2_2023)\
-str(q3_2023)\
-str(q4_2023)\
-str(q5_2023)\
-\
-#Combine all data frames into a single data frame\
-tripdata <- bind_rows(q6_2022, q7_2022, q8_2022, q9_2022, q10_2022, q11_2022, q12_2022, q1_2023, q2_2023, q3_2023, q4_2023, q5_2023)}
+1. RIDE SUMMARY OF EACH USER TYPE
+
+#COMPARISON BETWEEN MEMBERS CAUSAL RIDERS
+ride_summary <- tripdata_cleaned %>%
+  group_by(member_casual) %>% 
+  summarise(total_rides = n(),
+            total_rides_length = sum(ride_length),
+            average_ride_length = mean(ride_length),
+            median_length = median(ride_length), 
+            max_ride_length = max(ride_length),
+            min_ride_length = min(ride_length)) %>%
+  mutate(ride_percentage = total_rides/sum(total_rides) *100)
+ride_summary
+
+2.COMPARISON TOTAL AND AVERAGE RIDE LENGTH OF EACH USER TYPE BY DAY OF MONTH
+# First lets fix the days of the week order.
+tripdata_cleaned$month <- ordered(tripdata_cleaned$month, 
+                                  levels=c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
+
+# Calculate total number of rides and average ride length by user type and month
+rides_by_month <- tripdata_cleaned %>% 
+  group_by(month, member_casual, ) %>%  
+  summarise(number_of_rides = n(), average_ride_length = mean(ride_length), .groups="drop") %>% 
+  arrange(month, member_casual)
+rides_by_month
+
+3. COMPARISON TOTAL AND AVERAGE RIDE LENGTH OF EACH USER TYPE BY DAY OF WEEK
+#Fix the days of week
+tripdata_cleaned$day_of_week <- ordered(tripdata_cleaned$day_of_week, 
+                                        levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+#Total rides and average ride by day of week for each user type
+rides_by_day_of_week <- tripdata_cleaned %>%
+    group_by(day_of_week, member_casual) %>%
+    summarise(rides_by_day = n(), average_ride_length = mean(ride_length))
+
+rides_by_day_of_week
+
+4. CALCULATE PEEK PER HOUR IN A DAY AND PEEK RIDE PER HOUR IN A DAY BY DAY OF WEEK
+#Calculate peek ride by hour
+peak_ride_hours <- tripdata_cleaned %>%
+  group_by(member_casual,start_time) %>%
+  summarise(ride_count = n()) %>%
+  arrange(start_time,member_casual)
+peak_ride_hours
+
+5. CYCLISTIC ANALYSIS BY LOCATION
+# top 5 start station for all user
+ tripdata_cleaned %>%
+    count(start_station_name) %>%
+    arrange(desc(n)) %>%
+    head(n=5)
